0c87362355134376a45ead33a45d91ffdc742807
Juwon-06 <93054246+Juwon-06@users.noreply.github.com>
2022-03-23 17:10:58 -0700

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022cave",
+        "defaultValue":"2022",
 	"size":7,
         "required":"true",
 	"disabled":"true"
