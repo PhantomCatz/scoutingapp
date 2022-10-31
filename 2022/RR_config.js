d6d8ab7135c12820ffcd191010ffc3f879a6aece
Juwon-06 <93054246+Juwon-06@users.noreply.github.com>
2022-10-30 19:49:07 -0700

    Update RR_config.js
    carv to cabl

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022carv",
+        "defaultValue":"2022cabl",
 	"size":7,
         "required":"true",
 	"disabled":"true"
